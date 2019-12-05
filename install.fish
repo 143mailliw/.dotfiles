f0c8c139a92004b3462b74cce1610c8501c05ddc
william341 <wwokwilliam@gmail.com>
2019-12-04 22:32:49 -0700

    inc. fish config

--- install.fish
+++ install.fish
@@ -6,6 +6,16 @@ set_color normal
 ln -sf ~/.dotfiles/.config/* ~/.config/
 
 set_color green
+echo Install OMF
+set_color normal
+curl -L https://get.oh-my.fish | fish
+
+set_color green
+echo Installing bobthefish
+set_color normal
+omf install bobthefish
+
+set_color green
 echo Installing Hello theme
 set_color normal
 wget https://raw.githubusercontent.com/143mailliw/install-tool/patch-1/install.sh
